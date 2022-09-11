e00ac75c37a9fcbf14b823a01bd92ef875606edd
Shubhank-S <shubhanksharma345@gmail.com>
2022-09-11 17:03:30 +0530

    Porfolio done

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,17 @@
+import Todos from "../assets/Todos.png";
+import proj2 from "../assets/proj2.jpg";
+
+export const ProjectList = [
+  {
+    name: "Todos app",
+    image: Todos,
+    skills: "React JS,Material UI",
+    
+  },
+  {
+    name: "Ecommerce",
+    image: proj2,
+    skills: "React JS,Material UI",
+    
+  },
+];
